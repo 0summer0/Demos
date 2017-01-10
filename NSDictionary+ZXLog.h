fcd8b15a4f93db6ce4741287c3abb41990e51c85
Xiang Zhang <ggfx001@sina.com>
2017-01-10 20:35:40 +0800

    Unicode conversion

---
+++ NSDictionary+ZXLog.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+ZXLog.h
+//  ZX-UnicodeConversion
+//
+//  Created by shawn on 09/01/2017.
+//  Copyright © 2017 shawn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (ZXLog)
+
+@end
---
+++ NSDictionary+ZXLog.m
@@ -0,0 +1,66 @@
+//
+//  NSDictionary+ZXLog.m
+//  ZX-UnicodeConversion
+//
+//  Created by shawn on 09/01/2017.
+//  Copyright © 2017 shawn. All rights reserved.
+//
+
+#ifndef ZX_TARGET_NEED_UNICODE_CONVERSION
+#define ZX_TARGET_NEED_UNICODE_CONVERSION 1
+#endif
+
+#if ZX_TARGET_NEED_UNICODE_CONVERSION
+#import <objc/runtime.h>
+#endif
+#import "NSDictionary+ZXLog.h"
+
+@implementation NSDictionary (ZXLog)
+
+#if ZX_TARGET_NEED_UNICODE_CONVERSION
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        zx_swizzleSelector([self class], @selector(descriptionWithLocale:indent:), @selector(zx_descriptionWithLocale:indent:));
+    });
+}
+
+static inline void zx_swizzleSelector(Class theClass, SEL originalSelector, SEL swizzledSelector)
+{
+    Method originalMethod = class_getInstanceMethod(theClass, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(theClass, swizzledSelector);
+    
+    BOOL didAddMethod =
+    class_addMethod(theClass,
+                    originalSelector,
+                    method_getImplementation(swizzledMethod),
+                    method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+        class_replaceMethod(theClass,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+- (NSString *)zx_descriptionWithLocale:(id)locale indent:(NSUInteger)level
+{
+    return [self stringByReplaceUnicode:[self zx_descriptionWithLocale:locale indent:level]];
+}
+#endif
+
+- (NSString *)stringByReplaceUnicode:(NSString *)unicodeString
+{
+    NSMutableString *convertedString = [unicodeString mutableCopy];
+    [convertedString replaceOccurrencesOfString:@"\\U" withString:@"\\u" options:0 range:NSMakeRange(0, convertedString.length)];
+    CFStringRef transform = CFSTR("Any-Hex/Java");
+    CFStringTransform((__bridge CFMutableStringRef)convertedString, NULL, transform, YES);
+    
+    return convertedString;
+}
+
+@end
